f5d356754ac8e1c1798d516a5850d60bed2c1909
Abhishek Gupta <abhisheknik23@gmail.com>
2023-12-11 15:06:41 +0530

    -Added missing files and solve issues.

--- BhagavadGita/Injection.swift
+++ BhagavadGita/Injection.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Swinject
+
+final class Injection {
+
+    static let shared = Injection()
+
+    var container: Container {
+        get {
+            if _container == nil {
+                _container = buildContainer()
+            }
+            return _container!
+        }
+        set {
+            _container = newValue
+        }
+    }
+
+    private var _container: Container?
+
+    private func buildContainer() -> Container {
+        let contatiner = Container()
+        contatiner.register(BGLoggerType.self) { _ in
+            return BGLogger()
+        }
+        return contatiner
+    }
+}
+
+@propertyWrapper struct Injected<Dependency> {
+    let wrappedValue: Dependency
+
+    init(wrappedValue: Dependency) {
+        self.wrappedValue = Injection.shared.container.resolve(Dependency.self)!
+    }
+}
